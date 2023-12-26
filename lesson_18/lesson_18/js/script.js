08bcfa5812cd4600734e958f6beb418e73c9ef94
Yevhenii <53554718+zhen4ik10@users.noreply.github.com>
2023-12-26 16:35:00 +0200

    Add files via upload

---
+++ lesson_18/lesson_18/js/script.js
@@ -0,0 +1,9 @@
+// const icon = document.querySelector('.icon-menu');
+// icon.addEventListener('click', function () {
+//     document.documentElement.classList.toggle('open-menu');
+// });
+
+const icon = document.querySelector('.icon-menu');
+icon.addEventListener('click', function () {
+    document.documentElement.classList.toggle('open-menu');
+});
