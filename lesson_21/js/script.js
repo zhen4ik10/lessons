b76691d94b547a6e391e781510f783505a5f98e9
Yevhenii <53554718+zhen4ik10@users.noreply.github.com>
2024-01-10 00:21:20 +0200

    Add files via upload

--- lesson_21/js/script.js
+++ lesson_21/js/script.js
@@ -2,3 +2,8 @@ const icon = document.querySelector(".icon-menu");
 icon.addEventListener('click', function () {
     document.documentElement.classList.toggle('open-menu');
 });
+
+const iconSearch = document.querySelector(".form__button")
+iconSearch.addEventListener('click', function () {
+    document.documentElement.classList.toggle("open-input")
+})
