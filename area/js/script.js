aaf41251a09ea3d1b3dffd632383950eb9354e17
Yevhenii <53554718+zhen4ik10@users.noreply.github.com>
2024-01-23 10:47:20 +0200

    Add files via upload

---
+++ area/js/script.js
@@ -0,0 +1,9 @@
+const icon = document.querySelector(".icon-menu");
+icon.addEventListener('click', function () {
+    document.documentElement.classList.toggle('open-menu');
+});
+
+// const iconSearch = document.querySelector(".form__button")
+// iconSearch.addEventListener('click', function () {
+//     document.documentElement.classList.toggle("open-input")
+// })
